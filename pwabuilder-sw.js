ddd4b67ce36a53ac0db5c31f7ad1b7e1109c3991
Alok <alok9868@gmail.com>
2019-06-23 02:06:53 +0530

    add service worker

---
+++ pwabuilder-sw.js
@@ -0,0 +1,63 @@
+// This is the "Offline copy of pages" service worker
+
+const CACHE = "alok-offline";
+const offlineFallbackPage = "index.html";
+
+// Install stage sets up the index page (home page) in the cache and opens a new cache
+self.addEventListener("install", function (event) {
+  console.log("[Alok Prateek] Install Event processing");
+
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      console.log("[Alok Prateek] Cached offline page during install");
+
+      if (offlineFallbackPage === "index.html") {
+        return cache.add(new Response("offlineFallbackPage cached by the serviceworker."));
+      }
+      
+      return cache.add(offlineFallbackPage);
+    })
+  );
+});
+
+// If any fetch fails, it will look for the request in the cache and serve it from there first
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(function (response) {
+        console.log("[Alok Prateek] add page to offline cache: " + response.url);
+
+        // If request was success, add or update it in the cache
+        event.waitUntil(updateCache(event.request, response.clone()));
+
+        return response;
+      })
+      .catch(function (error) {        
+        console.log("[Alok Prateek] Network request Failed. Serving content from cache: " + error);
+        return fromCache(event.request);
+      })
+  );
+});
+
+function fromCache(request) {
+  // Check to see if you have it in the cache
+  // Return response
+  // If not in the cache, then return error page
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      if (!matching || matching.status === 404) {
+        return Promise.reject("no-match");
+      }
+
+      return matching;
+    });
+  });
+}
+
+function updateCache(request, response) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.put(request, response);
+  });
+}
