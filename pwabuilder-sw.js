9b25f3ecdb1e5fdf04f0ccd252e2f921a8255f20
Alok <alok9868@gmail.com>
2019-06-25 01:18:03 +0530

    change naming for better log

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -7,11 +7,11 @@ const offlineFallbackPage = "/offline.html";
 
 // Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener("install", function (event) {
-  console.log("[PWA Builder] Install Event processing");
+  console.log("[Alok Prateek - SW] Install Event processing");
 
   event.waitUntil(
     caches.open(CACHE).then(function (cache) {
-      console.log("[PWA Builder] Cached offline page during install");
+      console.log("[Alok Prateek - SW] Cached offline page during install");
       
       if (offlineFallbackPage === "ToDo-replace-this-name.html") {
         return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
@@ -29,7 +29,7 @@ self.addEventListener("fetch", function (event) {
   event.respondWith(
     fetch(event.request)
       .then(function (response) {
-        console.log("[PWA Builder] add page to offline cache: " + response.url);
+        console.log("[Alok Prateek - SW] add page to offline cache: " + response.url);
 
         // If request was success, add or update it in the cache
         event.waitUntil(updateCache(event.request, response.clone()));
@@ -37,7 +37,7 @@ self.addEventListener("fetch", function (event) {
         return response;
       })
       .catch(function (error) {
-        console.log("[PWA Builder] Network request Failed. Serving content from cache: " + error);
+        console.log("[Alok Prateek - SW] Network request Failed. Serving content from cache: " + error);
         return fromCache(event.request);
       })
   );
